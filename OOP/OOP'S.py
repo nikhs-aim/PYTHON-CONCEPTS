d2223e3188e56e3e7323dadf787037ca5284b51c
Nikhitha R karanth <nikhitharkaranth@gmail.com>
2023-10-04 16:46:39 +0530

    oop's concept

---
+++ OOP/OOP'S.py
@@ -0,0 +1,93 @@
+# solving a problem by creating objects
+# It implements DRY principle (Do not repeat yourself)
+# Class - blueprint for creating objects
+# Objects - instantiation of class
+# Objects of a given class can invoke its method without revealing the implementation details to the user - abstraction and encapsulation
+# Class attributes - attributes that belongs to a class rather than an object
+# Instance attributes - attributes that belongs to an instance
+
+
+# 1.RAILWAY EXAMPLE
+class RailwayForm:
+    formType = "Railway Form"
+
+    def printDta(self):
+        print(f"Name is {self.name}")
+        print(f"Train is {self.train}")
+
+
+nikhsApplication = RailwayForm()
+nikhsApplication.name = "nikhs"
+nikhsApplication.train = "Shatabdhi express"
+nikhsApplication.printDta()
+
+
+# 2.EMPLOYEE EXAMPLE
+class Employee:
+    company = 'Google'
+    salary = 100  # Instance attributes will be given importance, this is class attribute
+
+
+harry = Employee()
+nikhs = Employee()
+harry.salary = 300  # Instance attribute
+nikhs.salary = 400
+
+print(harry.company)
+print(nikhs.company)
+
+Employee.company = 'Youtube'  # class attribute
+
+print(harry.company)
+print(nikhs.company)
+
+print(harry.salary)
+print(nikhs.salary)
+
+
+# 3.SELF - If you want to represent an instance attribute or class attribute in a function.
+# We can pass arguments along with self.
+class Employee:
+    company = "Google"
+
+    def getSalary(self, sign):  # here self represents the object that you pass to the function, if you want to represent the attributes
+        print(
+            f"Salary for this employee in {self.company} is {self.salary},{sign}")
+
+
+nikhs = Employee()
+nikhs.salary = 100000
+nikhs.getSalary("Thanks!")  # Employee.getSalary(nikhs)
+
+
+# 4.static method
+''' if we want to use self, then we have to pass the object when the function is called.
+some times if we want to call a function without passing any objects, we should use @staticmethod'''
+
+
+class Employee:
+    @staticmethod
+    def greet():
+        print("Good morning")
+
+
+nikhs = Employee()
+nikhs.greet()
+
+
+# init method - this will run automatically, no need to call
+# we can also give arguments
+class Employee:
+    def __init__(self, name, company):
+        self.name = name  # set whatever we pass when called
+        self.company = company
+        print("Hello, how can I help you?")
+
+    def getDetails(self):
+        print(
+            f"The name of the employee is {self.name} and belongs to {self.company}")
+
+
+# objects can be instantiated like this using constructor
+nikhs = Employee("nikhs", "Google")
+nikhs.getDetails()
---
+++ OOP/practice.py
@@ -0,0 +1,133 @@
+# create a class programmer for storing info of few programers working at microsoft
+import sys
+
+
+class Programmer:
+    company = "Microsoft"
+
+    def __init__(self, name, product):
+        self.name = name
+        self.product = product
+
+    def getInfo(self):
+        print(
+            f"The name of the {self.company} programmer is {self.name} and the product is {self.product}")
+
+
+nikhs = Programmer("nikhs", "Skype")
+harry = Programmer("harry", "GitHub")
+nikhs.getInfo()
+harry.getInfo()
+
+
+# write a class calculator capable of finding square, cube and square root of a number
+# we can also initialize the number using __init__ method
+class Calculator:
+    def square(self, number):
+        print(f"Square of {number}: {number**2}")
+
+    def cube(self, number):
+        print(f"Cube of {number}: {number**3}")
+
+    def squareRoot(self, number):
+        print(f"Square root of {number}: {number**0.5}")
+
+
+calculate = Calculator()
+user = int(input('Enter number: '))
+calculate.square(user)
+calculate.cube(user)
+calculate.squareRoot(user)
+
+
+# Create a class with a class attribute a; create an object from it and set a directly using object a=0. Does this change the class attribute?
+# Answer is No
+class User:
+    a = "nikhs"
+
+    def printData(self):
+        print(f'User:{self.a}')
+
+
+person = User()
+person.a = "John"
+person.printData()
+print(User.a)
+
+
+# Add a static method in problem 2 to greet the user with hello
+class Calculator:
+
+    def __init__(self, number):
+        self.number = number
+
+    @staticmethod
+    def greet():
+        print('Welcome!')
+
+    def square(self):
+        print(f"Square of {self.number}: {self.number**2}")
+
+    def cube(self):
+        print(f"Cube of {self.number}: {self.number**3}")
+
+    def squareRoot(self):
+        print(f"Square root of {self.number}: {self.number**0.5}")
+
+
+user = int(input('Enter number: '))
+
+calculate = Calculator(user)
+calculate.greet()
+
+calculate.square()
+calculate.cube()
+calculate.squareRoot()
+
+
+# Write a class Train which has methods to book a ticket, get status ( no. of seats) and get fare information of trains running under Indian Railways
+
+
+class Train:
+    def __init__(self, name, fare, seat):
+        self.name = name
+        self.fare = fare
+        self.seat = seat
+
+    def getStatus(self):
+        print(f"The name of the train is {self.name}")
+        print(f"The seats available are {self.seat}")
+
+    def getFareInformation(self):
+        print(f"The price of the ticket is {self.fare}")
+
+    def bookTicket(self):
+        if (self.seat > 0):
+            print(
+                f'Your ticket has been booked! Your seat number is {self.seat}')
+            self.seat -= 1
+        else:
+            print('Sorry, there is no tickets available')
+
+    def cancelTicket(self, seat):
+        list_of_seats = []
+        for i in range(1, (self.seat)+1):
+            list_of_seats.append(i)
+
+        self.seat += 1
+        list_of_seats.append(seat)
+        print(list_of_seats)
+
+
+intercity = Train('intercity', 100, 4)
+intercity.getStatus()
+intercity.getFareInformation()
+intercity.bookTicket()
+intercity.getStatus()
+yes_or_no = input('Do you want to cancel this ticket? ')
+if yes_or_no == 'yes':
+    seat_number = int(input("Enter your seat number:"))
+    intercity.cancelTicket(seat_number)
+    intercity.getStatus()
+else:
+    sys.exit()
